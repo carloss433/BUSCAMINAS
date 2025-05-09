ea64e8289411075c3d787e81d545d19d5f4666d1
carloss433 <cavela8@gmail.com>
2025-05-09 17:16:55 +0200

    Las clases anteriores rellenas

--- src/ConjuntoJugadores.cpp
+++ src/ConjuntoJugadores.cpp
@@ -12,12 +12,190 @@
 
 #include "ConjuntoJugadores.h"
 
-ConjuntoJugadores::ConjuntoJugadores() {
+// Constructor con tamaño reservado
+ConjuntoJugadores::ConjuntoJugadores(int k) {
+    tamreservado = k;
+    vectorJugadores = new Jugador[tamreservado];
+    numjugadores = 0;
 }
 
+// Constructor por copia
 ConjuntoJugadores::ConjuntoJugadores(const ConjuntoJugadores& orig) {
+    tamreservado = orig.tamreservado;
+    numjugadores = orig.numjugadores;
+    vectorJugadores = new Jugador[tamreservado];
+    for (int i = 0; i < numjugadores; ++i) {
+        vectorJugadores[i] = orig.vectorJugadores[i];
+    }
 }
 
+// Destructor
 ConjuntoJugadores::~ConjuntoJugadores() {
+    delete[] vectorJugadores;
+}
+
+// Constructor con vector de nicks
+ConjuntoJugadores::ConjuntoJugadores(int n, string * vnicks) {
+    tamreservado = n;
+    numjugadores = n;
+    vectorJugadores = new Jugador[tamreservado];
+    for (int i=0; i<n; ++i) {
+        vectorJugadores[i] = Jugador(i + 1, vnicks[i]);
+    }
+}
+
+ConjuntoJugadores & ConjuntoJugadores::operator=(const ConjuntoJugadores & orig) {
+    if (this != &orig) {
+        delete[] vectorJugadores;
+        tamreservado = orig.tamreservado;
+        numjugadores = orig.numjugadores;
+        vectorJugadores = new Jugador[tamreservado];
+        for (int i=0; i<numjugadores; ++i) {
+            vectorJugadores[i] = orig.vectorJugadores[i];
+        }
+    }
+    return *this;
+}
+
+void ConjuntoJugadores::resize(int newtam) {
+    if (newtam > tamreservado) {
+        Jugador* nuevo = new Jugador[newtam];
+        for (int i=0; i<numjugadores; ++i) {
+            nuevo[i] = vectorJugadores[i];
+        }
+        delete[] vectorJugadores;
+        vectorJugadores = nuevo;
+        tamreservado = newtam;
+    }
+}
+
+void ConjuntoJugadores::ordenaporId() {
+    Jugador tmp;
+    for (int i=0; i<numjugadores - 1; ++i) {
+        for (int j=i+1; j<numjugadores; ++j) {
+            if (vectorJugadores[i].getId() > vectorJugadores[j].getId()) {
+                tmp = vectorJugadores[i];
+                vectorJugadores[i] = vectorJugadores[j];
+                vectorJugadores[j] = tmp;
+            }
+        }
+    }
+}
+
+ConjuntoJugadores & ConjuntoJugadores::operator+=(const Jugador& newjug) {
+    if (numjugadores == tamreservado) {
+        resize(tamreservado + 5);
+    }
+    vectorJugadores[numjugadores] = newjug;
+    numjugadores++;
+    ordenaporId();
+    return *this;
+}
+
+void ConjuntoJugadores::eliminaJugador(int jugid) {
+    int pos = buscaJugador(jugid);
+    if (pos != -1) {
+        for (int i = pos; i < numjugadores - 1; ++i) {
+            vectorJugadores[i] = vectorJugadores[i + 1];
+        }
+        numjugadores--;
+    }
+}
+
+int ConjuntoJugadores::buscaJugador(int jugid) {
+    for (int i=0; i<numjugadores; ++i) {
+        if (vectorJugadores[i].getId() == jugid) 
+            return i;
+    }
+    return -1;
+}
+
+int ConjuntoJugadores::buscaJugador(string nickbuscado) {
+    for (int i=0; i<numjugadores; ++i) {
+        if (vectorJugadores[i].getNick() == nickbuscado) 
+            return i;
+    }
+    return -1;
+}
+
+Jugador & ConjuntoJugadores::operator[](int i) const {
+    return vectorJugadores[i];
+}
+
+void ConjuntoJugadores::optimizar() {
+    Jugador* nuevo = new Jugador[numjugadores];
+    for (int i=0; i<numjugadores; ++i) {
+        nuevo[i] = vectorJugadores[i];
+    }
+    delete[] vectorJugadores;
+    vectorJugadores = nuevo;
+    tamreservado = numjugadores;
+}
+
+int ConjuntoJugadores::numJugadores() const {
+    return numjugadores;
+}
+
+string ConjuntoJugadores::mostrarRanking() {
+    // Ordenar copia del array manualmente por rendimiento
+    string resultado;
+    ConjuntoJugadores ranking(numjugadores);
+    Jugador tmp;
+    for(int i=0; i<numjugadores; i++){
+        ranking.vectorJugadores[i] = vectorJugadores[i];
+        }
+
+    for (int i=0; i<numjugadores - 1; ++i) {
+        for (int j=i+1; j<numjugadores; ++j) {
+
+
+            if (ranking.vectorJugadores[i].getRatio() < ranking.vectorJugadores[j].getRatio()) {
+                tmp = ranking.vectorJugadores[i];
+                ranking.vectorJugadores[i] = ranking.vectorJugadores[j];
+                ranking.vectorJugadores[j] = tmp;
+            }
+        }
+    }
+
+    for (int i=0; i<numjugadores; ++i) {
+        resultado += to_string(ranking.vectorJugadores[i].getId()) + " " + ranking.vectorJugadores[i].getNick() + " ";
+        resultado += to_string(ranking.vectorJugadores[i].numPartidasGanadas()) + "/" + to_string(ranking.vectorJugadores[i].numPartidasJugadas());
+        resultado += "\n";
+    }
+
+    return resultado;
+}
+
+std::ostream & operator<<(std::ostream & flujo, const ConjuntoJugadores & m) {
+    flujo << m.numjugadores << "\n";
+    for (int i=0; i<m.numjugadores; ++i) {
+        flujo << m.vectorJugadores[i] << "\n";
+    }
+    return flujo;
+}
+
+std::istream & operator>>(std::istream & flujo, ConjuntoJugadores & m) {
+    int n;
+    flujo >> n;
+    m.resize(n);
+    for (int i=0; i<n; ++i) {
+        int id, ganadas, perdidas;
+        string nick;
+        flujo >> id >> nick >> ganadas >> perdidas;
+        Jugador j(id, nick);
+        j.numPartidasGanadas() = ganadas;
+        j.numPartidasPerdidas() = perdidas;
+        m.vectorJugadores[i] = j;
+    }
+    return flujo;
+}
+
+ConjuntoJugadores operator+(const ConjuntoJugadores & left, const ConjuntoJugadores & right) {
+    ConjuntoJugadores nuevo(left.numjugadores + right.numjugadores);
+    for (int i=0; i<left.numjugadores; ++i)
+        nuevo += left.vectorJugadores[i];
+    for (int i=0; i<right.numjugadores; ++i)
+        nuevo += right.vectorJugadores[i];
+    return nuevo;
 }
 
--- src/Jugador.cpp
+++ src/Jugador.cpp
@@ -12,12 +12,113 @@
 
 #include "Jugador.h"
 
-Jugador::Jugador() {
-}
+// Constructor por defecto
+Jugador::Jugador() : id(0), nick(""), numpartidasganadas(0), numpartidasperdidas(0) {}
 
+// Constructor por copia
 Jugador::Jugador(const Jugador& orig) {
+    id = orig.id;
+    nick = orig.nick;
+    numpartidasganadas = orig.numpartidasganadas;
+    numpartidasperdidas = orig.numpartidasperdidas;
 }
 
+// Destructor
 Jugador::~Jugador() {
+    // No se usa memoria dinámica
 }
 
+// Constructor con id y nick
+Jugador::Jugador(int id, string nuevoNick) {
+    this->id = id;
+    this->nick = nuevoNick;
+    numpartidasganadas = 0;
+    numpartidasperdidas = 0;
+}
+
+string Jugador::getNick() {
+    return nick;
+}
+
+int& Jugador::numPartidasGanadas() {
+    return numpartidasganadas;
+}
+
+int& Jugador::numPartidasPerdidas() {
+    return numpartidasperdidas;
+}
+
+int Jugador::numPartidasJugadas() const {
+    return numpartidasganadas + numpartidasperdidas;
+}
+
+int Jugador::getId() const {
+    return id;
+}
+
+int& Jugador::setId() {
+    return id;
+}
+
+int Jugador::numPartidasGanadas() const {
+    return numpartidasganadas;
+}
+
+Jugador& Jugador::operator=(const Jugador& orig) {
+    if (this != &orig) {
+        id = orig.id;
+        nick = orig.nick;
+        numpartidasganadas = orig.numpartidasganadas;
+        numpartidasperdidas = orig.numpartidasperdidas;
+    }
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream& flujo, const Jugador& jug) {
+    flujo << jug.id << " " << jug.nick << " " << jug.numpartidasganadas << " " << jug.numPartidasJugadas();
+    return flujo;
+}
+
+// Operadores de comparación basados en proporción ganadas/jugadas
+
+bool operator==(const Jugador& izq, const Jugador& dch) {
+    int totalIzq = izq.numPartidasJugadas();
+    int totalDch = dch.numPartidasJugadas();
+    if (totalIzq == 0 && totalDch == 0) return true;
+    if (totalIzq == 0 || totalDch == 0) return false;
+    return (double)izq.numPartidasGanadas() / totalIzq == (double)dch.numPartidasGanadas() / totalDch;
+}
+
+
+bool operator!=(const Jugador& izq, const Jugador& dch) {
+    return !(izq == dch);
+}
+
+bool operator<(const Jugador& izq, const Jugador& dch) {
+    int totalIzq = izq.numPartidasJugadas();
+    int totalDch = dch.numPartidasJugadas();
+    if (totalIzq == 0 && totalDch == 0) return false;
+    if (totalIzq == 0) return true;
+    if (totalDch == 0) return false;
+    return (double)izq.numPartidasGanadas() / totalIzq < (double)dch.numPartidasGanadas() / totalDch;
+}
+
+bool operator>(const Jugador& izq, const Jugador& dch) {
+    return dch < izq;
+}
+
+bool operator<=(const Jugador& izq, const Jugador& dch) {
+    return !(dch < izq);
+}
+
+bool operator>=(const Jugador& izq, const Jugador& dch) {
+    return !(izq < dch);
+}
+
+double Jugador::getRatio() const {
+    int jugadas = numpartidasganadas+numpartidasperdidas;
+    if (jugadas == 0)
+        return 0.0;
+    else
+        return static_cast<double>(numpartidasganadas) / jugadas;
+}
